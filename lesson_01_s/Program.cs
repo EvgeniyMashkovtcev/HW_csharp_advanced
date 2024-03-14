1ab28d94c075bddd896317d30f64cdb6cc30d94f
evgenii_m <teken25424biz@gmail.com>
2024-03-14 22:29:51 +0300

    Добавьте файлы проекта.

---
+++ lesson_01_s/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Lesson_01_s
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double num1 = int.Parse(args[0]);
+            double num2 = int.Parse(args[2]);
+            string operation = args[1];
+
+            double result = 0;
+
+            if (operation == "+")
+            {
+                result = num1 + num2;
+            }
+            else if (operation == "-")
+            {
+                result = num1 - num2;
+            }
+            else if (operation == "/")
+            {
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                else
+                {
+                    Console.WriteLine("Деление на ноль");
+                    return;
+                }
+            }
+            else if (operation == "*")
+            {
+                result = num1 * num2;
+            }
+            else
+            {
+                Console.WriteLine("Неизвестная операция");
+                return;
+            }
+
+            Console.WriteLine($"Результат арифметической операции: {result}");
+        }
+
+    }
+}
